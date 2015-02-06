578780ebfb855a1f46cb00f8cdab2c51b5e932e4
Jason Aurich <jason@refund-retriever.com>
2015-02-06 02:33:49 -0600

    Halfway-point in ch 6 User model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true
+  validates :email, format: { with: /<regular expression>/ }
+
+end
