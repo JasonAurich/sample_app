ae0b58cc0f26c760a377d5bfe9f63f6eafc29097
Jason Aurich <jason@refund-retriever.com>
2015-03-01 20:07:58 -0600

    Near the end of Chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
 
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
@@ -12,9 +13,37 @@ class User < ActiveRecord::Base
 
   # Returns the hash digest of the given string.
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :  # Uses the minimum cost parameter in tests
+                                                  BCrypt::Engine.cost         # and a normal (high) cost parameter in production
+    BCrypt::Password.create(string, cost: cost)                               # 'String' is the string to be hashed and cost is
+                                                                              # the cost parameter that determines the
+                                                                              # computational cost to calculate the hash
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Because of the way Ruby handles assignments inside objects, without self the assignment would
+  # create a local variable called remember_token, which isn’t what we want. Using self ensures that
+  # assignment sets the user’s remember_token attribute.
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
 end
